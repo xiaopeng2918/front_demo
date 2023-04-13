56b7e7563265d506c33cc6338c805f20728d13ce
yxp2918 <2918369878@qq.com>
2023-04-13 13:50:23 +0800

    learn set_map

---
+++ js/ES6_new_type/1.set_map.js
@@ -0,0 +1,50 @@
+let mySet = new Set()
+// add value
+mySet.add(5) // number
+mySet.add({ name: 'yxp' })
+mySet.add(true)
+// no duplication eg:
+mySet.add(true)
+mySet.add(5)
+mySet.add(6)
+console.log(mySet) // Set(4) { 5, { name: 'yxp' }, true, 6 }
+// only one 5 and true
+
+//delete
+mySet.delete(5)
+console.log(mySet)
+// has
+const resultSetHas = mySet.has(true)
+console.log(resultSetHas) // true
+// typeof Set
+console.log(typeof mySet) // Object
+console.log(mySet instanceof Object) // true
+
+/**
+ *
+ * Map
+ */
+// set
+let myMap = new Map()
+myMap.set('name', 'yxp') // set为小写
+myMap.set(1, 5) // 键可以为数字
+console.log(myMap) // Map(2) { 'name' => 'yxp', 1 => 5 }
+// delete
+myMap.delete('name')
+console.log(myMap) // Map(1) { 1 => 5 }
+// has
+console.log(myMap.has(1)) // true
+console.log(myMap.has('name')) // false
+// get
+console.log(myMap.get(1)) // 5
+// forEach
+myMap.set('name', 'yxp')
+myMap.set('age', 18)
+myMap.set({}, { name: 'yxp' })
+myMap.set('name', 'yxp11')
+console.log(myMap) // Map(4) { 1 => 5, 'name' => 'yxp', 'age' => 18, {} => { name: 'yxp' } }
+
+myMap.forEach(function(key, value) {
+  console.log(key + ":" + value);
+})
+
